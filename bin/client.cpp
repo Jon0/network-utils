64f0f413cbae32f670f9f1259658a2c3a451cc15
Jon0 <Jono4728@gmail.com>
2016-05-15 17:33:10 +1200

    client

--- bin/client.cpp
+++ bin/client.cpp
@@ -4,20 +4,27 @@
 #include <libunix/socket.h>
 #include <libnet/protocol.h>
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        std::cout << "Use:\n\tnclient port\n";
-    }
 
-    auto ip = util::split<std::vector, std::string>(argv[1], ".");
+void test_connect(const std::string &addr) {
+    auto parts = util::split<std::vector, std::string>(addr, ".");
     std::vector<unsigned char> v;
-    for (auto &e: ip) {
+    for (auto &e: parts) {
         v.push_back(stoi(e));
     }
 
-    unix::IPv4 addr({v.at(0), v.at(1), v.at(2), v.at(3)});
-    std::cout << "connecting to " << addr.str() << "\n";
+    unix::IPv4 ip({v.at(0), v.at(1), v.at(2), v.at(3)});
+    std::cout << "connecting to " << ip.str() << "\n";
 
-    net::GroupRequest r(addr.connect(2620));
+    net::GroupRequest r(ip.connect(2620));
     r.request_neighbors();
+}
+
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cout << "Use:\n\tnclient address\n";
+        return 0;
+    }
+    test_connect(argv[1]);
+    return 0;
 }
